f8da35fde0867d377d7040513d7011be5d34a96c
Dave Dunkin <dave.dunkin@logos.com>
2024-08-29 08:27:01 -0700

    Add /env.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", (string? message) => message ?? "Hello World!");
-app.MapGet("/hash", (string? message) => Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(message ?? "Hello World!"))));
+app.MapGet("/hash", (string? message) => Convert.ToBase64String(SHA384.HashData(Encoding.UTF8.GetBytes(message ?? "Hello World!"))));
+app.MapGet("/env", () => new EnvironmentInfo());
 
 app.Run();
